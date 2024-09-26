d19d5c0808f2a6d7016e5f73e2f9b14ea3e1fe09
ArsenAvanesyan <01borzik@gmail.com>
2024-09-26 18:07:20 +0300

    feat: created store

--- client/src/app/store/index.ts
+++ client/src/app/store/index.ts
@@ -1,1 +1,11 @@
-//
+import { configureStore } from '@reduxjs/toolkit';
+import { userReducer } from '@/entities/user';
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
