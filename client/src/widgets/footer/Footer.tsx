2e2f5840703f3e418ba4ce4c055b929fc76cb7c4
sadmaloy <rostislav_ivanov2015@mail.ru>
2024-09-27 13:31:06 +0300

    feat: footer added

---
+++ client/src/widgets/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className={styles.container}>
+      <p>© 2024 Мне Этот Мир Абсолютно Понятен</p>
+    </footer>
+  );
+};
