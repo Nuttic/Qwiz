0bd3daacebfbd1769b47329d0dfb7b642fef5382
sadmaloy <rostislav_ivanov2015@mail.ru>
2024-09-27 14:16:32 +0300

    feat: questions are wrapped in a modal window and a button

--- client/src/pages/TopicPage/TopicPage.tsx
+++ client/src/pages/TopicPage/TopicPage.tsx
@@ -2,12 +2,14 @@ import Loader from "@/shared/ui/Loader/Loader";
 import React, { Suspense } from "react";
 
 const LazyTopicList = React.lazy(() => import("@/widgets/TopicList"));
+const LazyTopicItem = React.lazy(() => import("@/widgets/TopicItem"));
 
 export function TopicPage() {
   return (
     <div>
       <Suspense fallback={<Loader />}>
         <LazyTopicList />
+        <LazyTopicItem />
       </Suspense>
     </div>
   );
